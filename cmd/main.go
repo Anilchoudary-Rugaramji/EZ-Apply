8f7515ab5db9d7f16e6a96f7724de01b79f42f0e
anil <anilchoudary.r@gmail.com>
2025-03-06 22:28:15 +0100

    EZ: adding main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Anilchoudary-Rugaramji/EZ-Apply/handlers"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+
+	// create a gin router
+	router := gin.Default()
+
+	router.POST("/upload", handlers.UploadResume)
+
+	// start the server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Server running on port", port)
+	router.Run(":" + port)
+}
---
+++ internal/handlers/upload_handler.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Anilchoudary-Rugaramji/EZ-Apply/internal/storage"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func UplaoadResume(c *gin.Context) {
+
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+	// Get the uploaded file
+	file, fileHeader, err := c.Request.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No file uploaded"})
+		return
+	}
+	defer file.Close()
+
+	// Upload file to S3
+	fileURL, err := storage.UploadFile(file, fileHeader)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to upload file"})
+		return
+	}
+
+	// Return file URL
+	c.JSON(http.StatusOK, gin.H{
+		"message":  "Resume uploaded successfully",
+		"file_url": fileURL,
+	})
+}
